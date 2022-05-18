517bfb75f3d6f68f5396b34d829c3a74ef0de539
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-05-18 13:52:00 -0400

    restructured files and converted functions to arrow functions

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import './Card.css'
 
-function Card(props) {
+const Card = (props) => {
     const classes = 'card ' + props.className
     // props.children is the content inbetween opening and closing Card Tags
     return <div className={classes}>{props.children}</div>
